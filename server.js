7dfa9c9e9b9579d820daefa5a834442a7726863c
JoseB852 <jbetancourts976@gmail.com>
2026-06-02 16:08:22 -0400

    cambios

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const productosRoutes = require("./routes/productos");
+
+const app = express();
+
+// CORS (frontend puede conectar)
+app.use(cors());
+
+app.use(express.json());
+
+// Rutas
+app.use("/api/productos", productosRoutes);
+
+// PORT dinámico para Render
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
